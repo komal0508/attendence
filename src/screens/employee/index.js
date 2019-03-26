6a0497d8e61f6f97de12049e28418103731da8d1
komal0508 <kgoyal@innow8apps.com>
2019-03-26 12:18:41 +0530

    employee list
    -> Design Employee List Screen

--- src/screens/employee/index.js
+++ src/screens/employee/index.js
@@ -1,92 +1,65 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity,Platform, Alert, ScrollView, Image } from "react-native";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import styles from "./styles";
-import StatusBar from '../../common/statusBar';
-import TitleHeader from '../../common/titleHeader';
-import TitleCard from '../../common/titleCard';
-import Email from '../../images/Email.png';
-import DesignButton from '../../common/button'; 
-import FloatLabelTextField from '../../common/floatingLabelTextfield';
-import Delete from '../../images/Delete.png';
-import Edit from '../../images/Edit.png';
-import { deviceWidth, deviceHeight, invalid, valid, invalidEmail } from '../../common/constants';
-import Filter from '../../images/Filter.png';
-import Notification from '../../images/Notification.png';
+import StatusBar from "../../common/statusBar";
+import TitleHeader from "../../common/titleHeader";
+import TitleCard from "../../common/titleCard";
+import Email from "../../images/Email.png";
+import Delete from "../../images/Delete.png";
+import Edit from "../../images/Edit.png";
+import { deviceWidth, deviceHeight } from "../../common/constants";
+import Filter from "../../images/Filter.png";
+import Notification from "../../images/Notification.png";
 
 const empArray = [
-    { name: 'Komal',
-      empId: '1',
-    },
-    { name: 'Chirag Arora',
-    empId: '2',
-  },
-  { name: 'Komal Mittal',
-  empId: '3',
-},
-{ name: 'Sam',
-empId: '4',
-},
-{ name: 'Deepak',
-empId: '5',
-},
-{ name: 'Sunil',
-empId: '6',
-},
-{ name: 'Maninder',
-empId: '7',
-},
-{ name: 'Radhika',
-empId: '8',
-},
+  { name: "Komal", empId: "1" },
+  { name: "Chirag Arora", empId: "2" },
+  { name: "Komal Mittal", empId: "3" },
+  { name: "Sam", empId: "4" },
+  { name: "Deepak", empId: "5" },
+  { name: "Sunil", empId: "6" },
+  { name: "Maninder", empId: "7" },
+  { name: "Radhika", empId: "8" },
+  { name: "Gaurav", empId: "9" }
 ];
 
 class Employee extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        email: '',
-        password: '',
+      email: "",
+      password: ""
     };
-    this.toggleWalletList = this.toggleWalletList.bind(this);
-
+    this.selectCard = this.selectCard.bind(this);
   }
 
-  
-  toggleWalletList() {
-    // const { openWalletList } = this.state;
-    // this.setState({
-    //   openWalletList: !openWalletList,
-    // });
-  }
-  renderTitleCard() {
-    return  empArray.map((data, index) => (
-            <TitleCard
-            icon={Email}
-            titleCardMainViewStyle={styles.titleCardMainViewStyle}
-            titleCardImageStyle={styles.titleCardImageStyle}
-            titleCardTextStyle={styles.titleCardTextStyle}
-            titleCardHeadingStyle={styles.titleCardHeadingStyle}
-            titleMaterialTopIconStyle={styles.titleMaterialTopIconStyle}
-            titleMaterialBottomIconStyle={styles.titleMaterialBottomIconStyle}
-            name={data.name}
-            empId={data.empId}
-            onPress={this.toggleWalletList}
-            rightTopIcon={Edit}
-            rightBottomIcon={Delete}
-            imageTopViewStyle={{alignItems: 'center'}}
-            imageBottomViewStyle={{alignItems: 'center'}}
-            childViewStyle={styles.childViewStyle}
-          />
-          
-      ));
-  }
+  selectCard() {}
+
+  renderTitleCard = ({ item }) => (
+    <TitleCard
+      icon={Email}
+      titleCardMainViewStyle={styles.titleCardMainViewStyle}
+      titleCardImageStyle={styles.titleCardImageStyle}
+      titleCardTextStyle={styles.titleCardTextStyle}
+      titleCardHeadingStyle={styles.titleCardHeadingStyle}
+      titleMaterialTopIconStyle={styles.titleMaterialTopIconStyle}
+      titleMaterialBottomIconStyle={styles.titleMaterialBottomIconStyle}
+      name={item.name}
+      empId={item.empId}
+      onPress={this.selectCard}
+      rightTopIcon={Edit}
+      rightBottomIcon={Delete}
+      imageTopViewStyle={{ alignItems: "center" }}
+      imageBottomViewStyle={{ alignItems: "center" }}
+      childViewStyle={styles.childViewStyle}
+    />
+  );
 
   render() {
-    const { email, password } = this.state;
     return (
       <View style={styles.Container}>
-      <StatusBar />
-      <TitleHeader
+        <StatusBar />
+        <TitleHeader
           title="EMPLOYEE LIST"
           isBackArrow
           iconName="keyboard-arrow-left"
@@ -96,33 +69,23 @@ class Employee extends Component {
           rightIconType="ImageIconType"
           isRightDoubleIcon={true}
         />
-        <ScrollView
-          style={styles.scrollViewStyle}
+        <FlatList
+          style={{ marginTop: deviceHeight * 0.015 }}
           showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ alignItems: 'center' }}
-          >
-          <View style={{width: deviceWidth,  marginTop: deviceHeight * 0.05,}}>
-           {this.renderTitleCard()}
-          </View>
-          
-      <View style={styles.emptyViewStyle} />
-      </ScrollView>
-      <View style={{ 
-      width: deviceWidth, 
-      justifyContent: 'space-between', 
-      alignItems: 'center',
-       margin: deviceHeight * 0.01,
-       width: deviceWidth * 0.8, 
-       height: deviceHeight * 0.06,
-       backgroundColor: 'rgb(128, 0, 0)',
-        borderRadius: 5,
-       flexDirection: 'row',
-        }}>
-        <Text>Hello</Text>
-        <Text>Hello</Text>
-      </View>
-      </View>
+          data={empArray}
+          renderItem={this.renderTitleCard}
+        />
+        <View style={styles.bottomButtonStyle}>
+          <TouchableOpacity style={{ justifyContent: "center" }}>
+            <Text style={styles.buttonStyle}>Sort</Text>
+          </TouchableOpacity>
 
+          <View style={styles.seperator} />
+          <TouchableOpacity style={{ justifyContent: "center" }}>
+            <Text style={styles.buttonStyle}>Register</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
--- src/screens/employee/styles.js
+++ src/screens/employee/styles.js
@@ -1,16 +1,17 @@
 import { StyleSheet, Dimensions } from "react-native";
-const deviceWidth = Dimensions.get('window').width;
-const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get("window").width;
+const deviceHeight = Dimensions.get("window").height;
 
 export default StyleSheet.create({
   Container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: 'white',
+    alignItems: "center",
+    backgroundColor: "white"
   },
   scrollViewStyle: {
-    height: deviceHeight,
+    height: deviceHeight * 0.65,
     width: deviceWidth,
+    backgroundColor: "red"
   },
   emptyViewStyle: { height: deviceHeight * 0.05 },
   titleCardMainViewStyle: {
@@ -18,45 +19,66 @@ export default StyleSheet.create({
     width: deviceWidth * 0.9,
     paddingLeft: 15,
     height: deviceHeight * 0.15,
-    //backgroundColor: 'rgb(0, 177, 255)',
-    alignSelf: 'center',
+    alignSelf: "center",
     borderRadius: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     borderWidth: 1,
-    borderColor: 'rgb(128, 0, 0)'
+    borderColor: "rgb(128, 0, 0)"
   },
   titleCardImageStyle: {
     flex: 1,
     height: 30,
-    width: 30,
+    width: 30
   },
-  titleMaterialTopIconStyle:{
-    height: 15, 
+  titleMaterialTopIconStyle: {
+    height: 15,
     top: 15
   },
   titleMaterialBottomIconStyle: {
-      height: 15,
-      bottom: 15,
+    height: 15,
+    bottom: 15
   },
-  childViewStyle:{ backgroundColor: 'rgb(128, 0, 0)', 
-  borderBottomEndRadius: 5,
-   borderTopEndRadius: 5,
-   height: deviceHeight * 0.15, 
-   width: deviceWidth * 0.10,
-   justifyContent: 'space-between',},
+  childViewStyle: {
+    backgroundColor: "rgb(128, 0, 0)",
+    borderBottomEndRadius: 5,
+    borderTopEndRadius: 5,
+    height: deviceHeight * 0.15,
+    width: deviceWidth * 0.1,
+    justifyContent: "space-between"
+  },
   titleCardTextStyle: {
-  //  flex: 5,
-    color: 'black',
+    //  flex: 5,
+    color: "black",
     fontSize: 16,
     marginLeft: 10,
-    fontFamily: 'Roboto-Regular',
+    fontFamily: "Roboto-Regular"
   },
   titleCardHeadingStyle: {
-    color: 'black',
+    color: "black",
     fontSize: 16,
     marginLeft: 14,
-    fontFamily: 'Roboto-Regular',
+    fontFamily: "Roboto-Regular"
+  },
+  bottomButtonStyle: {
+    height: deviceHeight * 0.065,
+    margin: deviceHeight * 0.01,
+    width: deviceWidth * 0.8,
+    flexDirection: "row",
+    borderRadius: 5,
+    backgroundColor: "rgb(128, 0, 0)",
+    justifyContent: "space-between",
+    paddingHorizontal: deviceHeight * 0.07
+  },
+  buttonStyle: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+    fontFamily: "Roboto-Medium"
+  },
+  seperator: {
+    borderLeftWidth: 3,
+    borderLeftColor: "white"
   }
 });
